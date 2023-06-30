aa1f7054579472af9f6a41dfc1a13b57aeab10f5
Aryan Tiwari <87749380+aryantiwariofficial@users.noreply.github.com>
2023-06-30 19:49:18 +0530

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const Parser = require('parser')
+const app = express()
+
+
+app.use(parser())
+
+
+app.get('/numbers',res) => {
+  const schedule = ["ownerName", "rollNo", "ownerEmail","acesscode"]
+  res.json(number)
+})
 
